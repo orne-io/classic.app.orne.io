f97df062fb5409fd5a59d8f89e8c3c419a26050d
Romain Lanz <romain.lanz+github@pm.me>
2021-12-21 20:09:16 +0100

    chore: setup https, prettier and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import React from '@vitejs/plugin-react';
+import TSConfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [React(), TSConfigPaths()],
+
+	server: {
+		https: {
+			cert: process.env.LOCALHOST_HTTPS_CERT,
+			key: process.env.LOCALHOST_HTTPS_KEY,
+		},
+	},
+});
