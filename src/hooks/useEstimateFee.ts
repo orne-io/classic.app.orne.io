e1ee2057b141a6d0e5e5355e8d61561397a49269
Romain Lanz <romain.lanz+github@pm.me>
2022-01-16 16:13:34 +0100

    refactor(hooks): update useEstimateFee to use React Query

--- src/hooks/useEstimateFee.ts
+++ src/hooks/useEstimateFee.ts
@@ -1,16 +1,30 @@
-import { useCallback } from 'react';
+import Decimal from 'decimal.js';
+import { useMutation } from 'react-query';
+import { Coin, MsgExecuteContract } from '@terra-money/terra.js';
 import { useConnectedWallet, useLCDClient } from '@terra-money/wallet-provider';
-import type { Msg } from '@terra-money/terra.js';
+import { useApp } from 'hooks/useApp';
+import { computeSwapUstToOrneMessage } from 'hooks/useSwap';
+
+type SwapParams = { slippage: string } & (SwapUstParams | SwapOrneParams);
+type SwapUstParams = { amountUst: string };
+type SwapOrneParams = { amountOrne: string };
 
 export function useEstimateFee() {
 	const lcd = useLCDClient();
 	const connectedWallet = useConnectedWallet();
+	const { contractAddress } = useApp();
 
-	return useCallback(
-		async (msgs: Msg[]) => {
+	const { mutateAsync } = useMutation(async (params: SwapParams) => {
+		if ('amountUst' in params) {
+			const query = computeSwapUstToOrneMessage(params.amountUst, params.slippage);
+
+			const msg = new MsgExecuteContract(connectedWallet!.walletAddress, contractAddress.pair, query, [
+				new Coin('uusd', new Decimal(params.amountUst).times(1_000_000)),
+			]);
+
 			const accountInfo = await lcd.auth.accountInfo(connectedWallet!.walletAddress);
 
-			return await lcd.tx.estimateFee(
+			return lcd.tx.estimateFee(
 				[
 					{
 						publicKey: accountInfo.getPublicKey(),
@@ -18,13 +32,16 @@ export function useEstimateFee() {
 					},
 				],
 				{
-					msgs,
+					msgs: [msg],
 					feeDenoms: ['uusd'],
 					gasPrices: '0.456uusd',
 					gasAdjustment: 1.6,
 				}
 			);
-		},
-		[lcd, connectedWallet]
-	);
+		}
+	});
+
+	return {
+		estimate: mutateAsync,
+	};
 }
