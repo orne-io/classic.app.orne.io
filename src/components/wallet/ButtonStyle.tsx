15b785670ad61a80090eba25f680505b81706c53
Romain Lanz <romain.lanz@pm.me>
2022-03-04 23:27:24 +0100

    feat: remove styled component

--- src/components/wallet/ButtonStyle.tsx
+++ src/components/wallet/ButtonStyle.tsx
@@ -1,66 +1,63 @@
-import styled from 'styled-components';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { styled } from 'stitches.config';
 
-export const Trigger = styled(DropdownMenu.Trigger)`
-	align-items: center;
-	background-color: var(--darker-green);
-	border-radius: var(--rounded);
-	color: white;
-	display: inline-flex;
-	padding: var(--space-1) var(--space-2);
-`;
+export const Trigger = styled(DropdownMenu.Trigger, {
+	alignItems: 'center',
+	backgroundColor: '$darkGreen',
+	borderRadius: '$rounded',
+	color: 'white',
+	display: 'inline-flex',
+	padding: '$1 $2',
+});
 
-export const Balance = styled.span`
-	display: inline-block;
-	margin-right: var(--space-3);
+export const Balance = styled('span', {
+	'display': 'inline-block',
+	'marginRight': '$3',
+	'@media screen and (max-width: 768px)': {
+		fontSize: '1rem',
+	},
+});
 
-	@media screen and (max-width: 768px) {
-		font-size: 1rem;
-	}
-`;
+export const WalletIcon = styled('img', {
+	height: '20px',
+});
 
-export const WalletIcon = styled.img`
-	height: 20px;
-`;
+export const WalletAddress = styled('span', {
+	'alignItems': 'center',
+	'backgroundColor': 'white',
+	'borderRadius': '$rounded',
+	'color': '$textColor',
+	'display': 'inline-flex',
+	'gap': '$2',
+	'padding': '$1 $3',
 
-export const WalletAddress = styled.span`
-	align-items: center;
-	background-color: white;
-	border-radius: var(--rounded);
-	color: var(--text-color);
-	display: inline-flex;
-	gap: var(--space-2);
-	padding: var(--space-1) var(--space-3);
+	'@media screen and (max-width: 768px)': {
+		fontSize: '1rem',
+	},
+});
 
-	@media screen and (max-width: 768px) {
-		font-size: 1rem;
-	}
-`;
+export const WalletAddressString = styled('span', {
+	'@media screen and (max-width: 768px)': {
+		display: 'none',
+	},
+});
 
-export const WalletAddressString = styled.span`
-	@media screen and (max-width: 768px) {
-		display: none;
-	}
-`;
+export const Content = styled(DropdownMenu.Content, {
+	backgroundColor: 'white',
+	borderRadius: '$rounded',
+	boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1)',
+	marginTop: '$2',
+	paddingBlock: '$1',
+	width: '250px',
+});
 
-export const Content = styled(DropdownMenu.Content)`
-	background-color: white;
-	border-radius: var(--rounded);
-	box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1);
+export const Item = styled(DropdownMenu.Item, {
+	'cursor': 'pointer',
+	'padding': '$1 $3',
+	'userSelect': 'none',
+	'outline': 'none',
 
-	margin-top: var(--space-2);
-	padding-block: var(--space-1);
-	width: 250px;
-`;
-
-export const Item = styled(DropdownMenu.Item)`
-	cursor: pointer;
-	padding: var(--space-1) var(--space-3);
-	user-select: none;
-	outline: none;
-
-	&:focus,
-	&:hover {
-		background-color: var(--light-green);
-	}
-`;
+	'&:focus, &:hover': {
+		backgroundColor: '$lightGreen',
+	},
+});
